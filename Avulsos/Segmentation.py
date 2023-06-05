c6a517fd7ccb3a16dc3ac6628af5a0f66fcb2cd2
Pedro <pedro.borges@estudante.ufscar.br>
2023-06-05 15:14:18 -0300

    Create Segmentation.py

---
+++ Avulsos/Segmentation.py
@@ -0,0 +1,14 @@
+import numpy as np
+from glob import glob
+from PIL import Image
+from matplotlib import pyplot as plt
+from felzenszwalb_segmentation import segment
+image_files = glob('/users/ADM/desktop/imagens/*')
+image = np.array(Image.open(image_files[1]))
+segmented_image = segment(image, 0.2, 400, 50)
+fig = plt.figure(figsize=(12,12))
+a = fig.add_subplot(1, 2, 1)
+plt.imshow(image)
+a = fig.add_subplot(1, 2, 2)
+plt.imshow(segmented_image.astype(np.uint8))
+plt.show()
