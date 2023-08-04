f9ab6846f3248c21c724ee6e047cf57f37082f11
Pedro <pedroborges.itapira@gmail.com>
2023-08-04 14:48:11 -0300

    Adicionar T5.2

--- Avulsos/T52.py
+++ Avulsos/T52.py
@@ -1,28 +1,41 @@
-import numpy
+import math
 
 def f(x):
     # Defina a função que deseja integrar aqui
-    return x**2
+    return math.exp(-x**2)
+
+def truncate(x):
+    return int(x * 10**8) / 10**8
 
 def simpson_integral(a, b, n):
-    h = (b - a) / n
-    integral = f(a) + f(b)
+    h = (b - a) / (2*n)
+    integral_sum = f(a)  # First term: f(a)
+    k = 4
+    for i in range(1, 2*n):
+        xi = a + (i * h)
+        print(xi)
+        integral_sum += (k * f(xi))  # Middle terms: f(x_i)
+        k = (k+2)%4
+        if k==0: k = 4
+    integral_sum += f(b)  # Last term: f(b)/2
+    integral_result = h * integral_sum / 3
+    return integral_result
 
-    for i in range(1, n, 2):
-        integral += 4 * f(a + i * h)
-
-    for i in range(2, n-1, 2):
-        integral += 2 * f(a + i * h)
-
-    integral *= h / 3
-    return integral
+def simpsoncprecisão(a, b):
+    n = 2
+    while True:
+        integral_n_minus_1 = simpson_integral(a, b, n - 1)
+        integral_n = simpson_integral(a, b, n)
+        if truncate(integral_n_minus_1) == truncate(integral_n):
+            return integral_n, n
+        n += 1
+        if n >= 100:
+            return integral_n, n
 
 # Intervalo de integração
-a = -1,2
-b = 2,2
+a = -1.2
+b = 2.2
 
-# Número de subintervalos (deve ser um número ímpar)
-n = 10000
-
-integral_value = simpson_integral(a, b, n)
-print(f"O valor numérico da integral é: {integral_value:.8f}")
+integral_value, n = simpsoncprecisão(a, b)
+print(f"O valor numérico da integral é: {integral_value}")
+print(n)
