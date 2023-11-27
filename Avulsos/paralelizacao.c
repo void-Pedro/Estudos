ff4c0776407a0b8cf21437dc3a3f5e67d1abf9ba
Pedro <pedro.borges@estudante.ufscar.br>
2023-11-27 14:47:17 -0300

    Comentarios

--- Avulsos/paralelizacao.c
+++ Avulsos/paralelizacao.c
@@ -2,6 +2,9 @@
 ** PPD / DC/UFSCar - Helio
 ** Programa : multiplicacao de matrizes
 ** Objetivo: paralelizacao om OpenMP
+** Alunos: Pedro Henrique Borges - 804071
+** Heitor Colichio - 800423
+
 */
 
 #include <math.h>
@@ -47,16 +50,28 @@ main(int argc, char *argv[])
 	// calculo da multiplicacao
  
 	// Qual/quais loop(s) paralelizar? Vale a pena paralelizar todos?
-	// Qual é o efeito de fazer um parallel for em cada um dos fors abaixo?
-	// É necessários sincronizar alguma operação, garantindo exclusão mútua?
-	
-	for(i=0; i < lin_c; i++) {
+	// Tendo em vista os tempos de execução dos testes feitos acima, percebe-se que paralelizar os loops mais externos
+	// é mais eficiente, já que há um salto absurdo de performance do loop intermediário para o mais interno. 
 
+	// Qual é o efeito de fazer um parallel for em cada um dos fors abaixo?
+	// Utilizando o parallel for em cada um dos loops, paraleliza-se suas iterações (dos loops aninhados também), dividindo a carga de trabalho em várias threads.
+
+	// É necessários sincronizar alguma operação, garantindo exclusão mútua?
+	// Não foi necessário, já que não havia um valor que dependia de outro
+
+	// Paralelizando no loop mais externo
+	//#pragma omp parallel for private(j,k)
+	for(i=0; i < lin_c; i++) {
+		
+		// Paralelizando no loop intermediário
+		// #pragma omp parallel for private(k)
 		for(j=0; j < col_c; j++) {
 			// pode ser útil usar uma variável auxiliar para os cálculos
 			C[i*col_c+j]=0;
 
-			#pragma omp parallel for
+			// Paralelização no loop mais interno, testando também a utilização do reduction
+			// #pragma omp parallel for
+			#pragma omp parallel for reduction(+:)
 			for(k=0; k < col_a; k++) 
 				C[i*col_c+j] += A[i*col_a+k] * B[k*col_b+j];
        // ser usou variável auxiliar, atribui-se seu valor à C[i][j]
